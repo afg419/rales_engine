66ab6703aea2a5edeb01567760e57a0cb3371882
Aaron Greenspan <afg419@gmail.com>
2016-02-22 16:12:31 -0700

    Databases loaded sir.

---
+++ spec/database/rake_spec.rb
@@ -0,0 +1,11 @@
+describe "loads the database correctly" do
+
+  # it "loads customers correctly" do
+  #
+  # end
+  #
+  # it "loads customers correctly2" do
+  #   expect(1+1).to eq 2
+  # end
+
+end
