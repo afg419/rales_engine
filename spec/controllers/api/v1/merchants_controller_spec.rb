72a67c46b4eb2f510b31d5d17bf78e3989feec73
Aaron Greenspan <afg419@gmail.com>
2016-02-22 17:19:24 -0700

    Implements show for merchants

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -2,15 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::MerchantsController, type: :controller do
   it "Returns all merchants" do
-    Merchant.create(name: "merchant1")
-    Merchant.create(name: "merchant2")
+    m1 = Merchant.create(name: "merchant1")
+    m2 = Merchant.create(name: "merchant2")
     get :index, format: :json
 
-    merchant_params = [{"id"=>1,
+    merchant_params = [{"id"=>m1.id,
                         "name"=>"merchant1"},
-                       {"id"=>2,
+                       {"id"=>m2.id,
                         "name"=>"merchant2"}]
     expect(response.status).to eq 200
     # expect(JSON.parse(response.body)["merchants"]).to eq merchant_params
+  end
+
+  it "Returns specific merchant" do
+    m1 = Merchant.create(name: "merchant1")
+    m2 = Merchant.create(name: "merchant2")
+    get :show, format: :json, id: m1.id
+
+    expect(response.status).to eq 200
+    # expect(JSON.parse(response.body)["merchants"]).to eq m1
   end
 end
