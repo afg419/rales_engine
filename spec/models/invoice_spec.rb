7368eefc25221995e54f261b197738ee23de41a9
Aaron Greenspan <afg419@gmail.com>
2016-02-24 15:13:37 -0700

    Fully tests models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "computes pending invoices" do
+    t1 = Transaction.create(result: "success")
+    t2 = Transaction.create(result: "failed")
+    i1 = Invoice.create(transactions: [t1]).id
+    i2 = Invoice.create(transactions: [t2]).id
+    i3 = Invoice.create.id
+
+    expected = [i2, i3]
+    computed = Invoice.pending.map(&:id)
+    expect(expected).to eq computed
+  end
 end
